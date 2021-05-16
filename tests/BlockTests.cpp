e160b8cd3c95eca13c209716c3c79b6e64f17401
Nicholas Corgan <n.corgan@gmail.com>
2021-05-16 10:05:34 -0500

    Tests work

--- tests/BlockTests.cpp
+++ tests/BlockTests.cpp
@@ -7,19 +7,133 @@
 #include <Pothos/Framework.hpp>
 #include <Pothos/Testing.hpp>
 
+#include <cmath>
+#include <iostream>
+#include <type_traits>
+
 //
 // /volk/acos
 //
 
 POTHOS_TEST_BLOCK("/volk/tests", test_acos)
 {
-    std::vector<float> testInputs;
-    std::vector<float> expectedOutputs;
+    std::vector<float> testInputs      = {0.0f, 0.5f, 1.0f};
+    std::vector<float> expectedOutputs = {M_PI/2.0f, M_PI/3.0f, 0.0f};
 
-    auto acos = Pothos::BlockRegistry::make("/volk/acos", Pothos::DType(typeid(float)));
+    auto acos = Pothos::BlockRegistry::make("/volk/acos");
 
     VOLKTests::testOneToOneBlock<float,float>(
         acos,
         testInputs,
+        expectedOutputs);
+}
+
+//
+// /volk/asin
+//
+
+POTHOS_TEST_BLOCK("/volk/tests", test_asin)
+{
+    std::vector<float> testInputs      = {0.0f, 0.5f, 1.0f};
+    std::vector<float> expectedOutputs = {0.0f, M_PI/6.0f, M_PI/2.0f};
+
+    auto asin = Pothos::BlockRegistry::make("/volk/asin");
+
+    VOLKTests::testOneToOneBlock<float,float>(
+        asin,
+        testInputs,
+        expectedOutputs);
+}
+
+//
+// /volk/atan
+//
+
+POTHOS_TEST_BLOCK("/volk/tests", test_atan)
+{
+    std::vector<float> testInputs      = {0.0f, 1.0f, INFINITY};
+    std::vector<float> expectedOutputs = {0.0f, M_PI/4.0f, M_PI/2.0f};
+
+    auto atan = Pothos::BlockRegistry::make("/volk/atan");
+
+    VOLKTests::testOneToOneBlock<float,float>(
+        atan,
+        testInputs,
+        expectedOutputs);
+}
+
+//
+// /volk/add
+//
+
+template <typename InType0, typename InType1, typename OutType>
+static void getAddTestValues(
+    std::vector<InType0>* pTestInputs0,
+    std::vector<InType1>* pTestInputs1,
+    std::vector<OutType>* pExpectedOutputs)
+{
+    *pTestInputs0 = {InType0(1), InType0(2), InType0(3)};
+    *pTestInputs1 = {InType1(4), InType1(5), InType1(6)};
+    *pExpectedOutputs = {OutType(5), OutType(7), OutType(9)};
+}
+
+template <typename InType0, typename InType1, typename OutType>
+static void testAdd()
+{
+    Pothos::DType inDType0(typeid(InType0));
+    Pothos::DType inDType1(typeid(InType1));
+    Pothos::DType outDType(typeid(OutType));
+
+    std::cout << " * Testing " << inDType0.name() << " + "
+              << inDType1.name() << " = " << outDType.name()
+              << "..." << std::endl;
+
+    std::vector<InType0> testInputs0;
+    std::vector<InType1> testInputs1;
+    std::vector<OutType> expectedOutputs;
+    getAddTestValues(
+        &testInputs0,
+        &testInputs1,
+        &expectedOutputs);
+    
+    auto add = Pothos::BlockRegistry::make(
+        "/volk/add",
+        inDType0,
+        inDType1,
+        outDType);
+
+    VOLKTests::testTwoToOneBlock<InType0,InType1,OutType>(
+        add,
+        testInputs0,
+        testInputs1,
+        expectedOutputs);
+}
+
+POTHOS_TEST_BLOCK("/volk/tests", test_add)
+{
+    testAdd<float,float,float>();
+    testAdd<float,double,double>();
+    testAdd<double,double,double>();
+    testAdd<std::complex<float>,std::complex<float>,std::complex<float>>();
+}
+
+//
+// /volk/and
+//
+
+POTHOS_TEST_BLOCK("/volk/tests", test_and)
+{
+    std::vector<int> testInputs0     = {123,456,789};
+    std::vector<int> testInputs1     = {321,654,987};
+    std::vector<int> expectedOutputs = {testInputs0[0] & testInputs1[0],
+                                        testInputs0[1] & testInputs1[1],
+                                        testInputs0[2] & testInputs1[2]};
+
+    auto andBlock = Pothos::BlockRegistry::make("/volk/and");
+
+    VOLKTests::testTwoToOneBlock<int,int,int>(
+        andBlock,
+        testInputs0,
+        testInputs1,
         expectedOutputs);
 }
--- tests/BlockTests.hpp
+++ tests/BlockTests.hpp
@@ -14,6 +14,54 @@ namespace VOLKTests
 {
     static constexpr size_t NumRepetitions = 256;
 
+    template <typename T>
+    static constexpr T epsilon()
+    {
+        return T(0);
+    }
+
+    template <>
+    constexpr float epsilon<float>()
+    {
+        return 1e-3f;
+    }
+
+    template <>
+    constexpr double epsilon<double>()
+    {
+        return 1e-3;
+    }
+
+    template <typename T>
+    DisableIfComplex<T, void> testBufferChunks(
+        const Pothos::BufferChunk& expected,
+        const Pothos::BufferChunk& actual)
+    {
+        testBufferChunksClose<T>(
+            expected,
+            actual,
+            epsilon<T>());
+    }
+
+    template <typename T>
+    EnableIfComplex<T, void> testBufferChunks(
+        const Pothos::BufferChunk& expected,
+        const Pothos::BufferChunk& actual)
+    {
+        using ScalarType = typename T::value_type;
+        static const Pothos::DType dtype(typeid(ScalarType));
+
+        auto expectedScalar = Pothos::BufferChunk(expected);
+        expectedScalar.dtype = dtype;
+
+        auto actualScalar = Pothos::BufferChunk(actual);
+        actualScalar.dtype = dtype;
+
+        testBufferChunks<ScalarType>(
+            expectedScalar,
+            actualScalar);
+    }
+
     template <typename InType, typename OutType>
     void testOneToOneBlock(
         const Pothos::Proxy& testBlock,
@@ -44,8 +92,53 @@ namespace VOLKTests
             POTHOS_TEST_TRUE(topology.waitInactive(0.01));
         }
 
-        auto outputs = sink.call<Pothos::BufferChunk>("feedBuffer");
-        testBufferChunksEqual<OutType>(
+        auto outputs = sink.call<Pothos::BufferChunk>("getBuffer");
+        testBufferChunks<OutType>(
+            expectedOutputs,
+            outputs);
+    }
+
+    template <typename InType0, typename InType1, typename OutType>
+    void testTwoToOneBlock(
+        const Pothos::Proxy& testBlock,
+        const std::vector<InType0>& testInputs0Vec,
+        const std::vector<InType1>& testInputs1Vec,
+        const std::vector<OutType>& expectedOutputsVec)
+    {
+        static const Pothos::DType InDType0(typeid(InType0));
+        static const Pothos::DType InDType1(typeid(InType1));
+        static const Pothos::DType OutDType(typeid(OutType));
+
+        const auto testInputs0 = VOLKTests::stdVectorToStretchedBufferChunk(
+            testInputs0Vec,
+            NumRepetitions);
+        const auto testInputs1 = VOLKTests::stdVectorToStretchedBufferChunk(
+            testInputs1Vec,
+            NumRepetitions);
+        const auto expectedOutputs = VOLKTests::stdVectorToStretchedBufferChunk(
+            expectedOutputsVec,
+            NumRepetitions);
+
+        auto source0 = Pothos::BlockRegistry::make("/blocks/feeder_source", InDType0);
+        source0.call("feedBuffer", testInputs0);
+
+        auto source1 = Pothos::BlockRegistry::make("/blocks/feeder_source", InDType1);
+        source1.call("feedBuffer", testInputs1);
+
+        auto sink = Pothos::BlockRegistry::make("/blocks/collector_sink", OutDType);
+
+        {
+            Pothos::Topology topology;
+            topology.connect(source0, 0, testBlock, 0);
+            topology.connect(source1, 0, testBlock, 1);
+            topology.connect(testBlock, 0, sink, 0);
+
+            topology.commit();
+            POTHOS_TEST_TRUE(topology.waitInactive(0.01));
+        }
+
+        auto outputs = sink.call<Pothos::BufferChunk>("getBuffer");
+        testBufferChunks<OutType>(
             expectedOutputs,
             outputs);
     }
